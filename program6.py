aed4f45471e1b768b5b6d4b7819756ab945133fd
Immunity98 <zampolyt_67@mail.ru>
2015-06-30 11:34:49 +0300

    first commit

---
+++ program6.py
@@ -0,0 +1,26 @@
+#входные данные: число (факториал какого-то числа)
+#выходные данные: число (то, с которого этот факториал получился)
+#пример. входные данные: 120, результат: 5 (max =1000) ==> точные числа
+print ('Enter any chislo factorial')
+a = int(input())
+i=1
+s=1
+if a>1000:
+          exit
+if a % 2 ==0:
+          while s <a:
+                    if s<a:
+                              a=a/i
+                              i=i+1
+                    else:
+                              i=1
+          if i>1:
+                    i=i-1
+          if a<=0:
+                    a=1
+                    i=1
+          print ('chislo factorial=', i)
+else:
+          print ('Error #144')
+input()
+
